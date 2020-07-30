758e42f80fd108e97676b683da10fe670f51c549
Admin <mariaphillips2323@gmail.com>
2020-07-30 19:02:24 -0400

    KatalonAPI

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5e7f22fe-acd5-4d64-9034-fe09059674b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>a24ef1d9-8fa1-4ff1-8338-ffebd49ccbad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUser/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
